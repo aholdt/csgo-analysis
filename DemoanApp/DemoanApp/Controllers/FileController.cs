836f7868a2befb52da076003207865841683e033
Alexander Holdt <ave@cvation.com>
2021-07-28 20:52:17 +0200

    add dummy fileupload endpoint

---
+++ DemoanApp/DemoanApp/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using DemoanApp.PublicModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoanApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        [HttpPost]
+        public Task UploadDemo([FromForm] FileUpload fileUpload)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
