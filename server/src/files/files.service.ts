0c13ef8100632f595d1f97322f590287de0f43b3
Alexander Holdt <ave@cvation.com>
2021-08-01 16:07:38 +0200

    Change blobserviceclient to only be created on upload

--- server/src/files/files.service.ts
+++ server/src/files/files.service.ts
@@ -1,28 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { BlobServiceClient, ContainerClient} from '@azure/storage-blob';
+import { Injectable } from "@nestjs/common";
+import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
 
 @Injectable()
 export class FilesService {
-    blobServiceClient: BlobServiceClient;
-    
-    constructor() {
-        // Create Blob Service Client from Account connection string or SAS connection string
-        // Account connection string example - `DefaultEndpointsProtocol=https;AccountName=myaccount;AccountKey=accountKey;EndpointSuffix=core.windows.net`
-        // SAS connection string example - `BlobEndpoint=https://myaccount.blob.core.windows.net/;QueueEndpoint=https://myaccount.queue.core.windows.net/;FileEndpoint=https://myaccount.file.core.windows.net/;TableEndpoint=https://myaccount.table.core.windows.net/;SharedAccessSignature=sasString`
-        const STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING || "";
-        this.blobServiceClient = BlobServiceClient.fromConnectionString(STORAGE_CONNECTION_STRING);
-    }
-    async uploadFile(file: Express.Multer.File) : Promise<void> {
-        const container = await this.GetOrCreateContainer();
-        console.log(file);
-        const blobClient = container.getBlockBlobClient(file.originalname)
-        console.log("uploading file " + file.filename);
-        await blobClient.uploadData(file.buffer, {blobHTTPHeaders: { blobContentType: file.mimetype }});
-    }
+  blobServiceClient: BlobServiceClient;
 
-    private async GetOrCreateContainer(): Promise<ContainerClient> {
-        const container = await this.blobServiceClient.getContainerClient("unparsed-demos");
-        await container.createIfNotExists();
-        return container;
+  async uploadFile(file: Express.Multer.File): Promise<void> {
+    this.EnsureBlobServiceClient();
+    const container = await this.GetOrCreateContainer();
+    console.log(file);
+    const blobClient = container.getBlockBlobClient(file.originalname);
+    console.log("uploading file " + file.filename);
+    await blobClient.uploadData(file.buffer, {
+      blobHTTPHeaders: { blobContentType: file.mimetype },
+    });
+  }
+
+  private EnsureBlobServiceClient() {
+    const STORAGE_CONNECTION_STRING =
+      process.env.AZURE_STORAGE_CONNECTION_STRING || "";
+    if (!this.blobServiceClient) {
+      this.blobServiceClient = BlobServiceClient.fromConnectionString(
+        STORAGE_CONNECTION_STRING
+      );
     }
+  }
+
+  private async GetOrCreateContainer(): Promise<ContainerClient> {
+    const container = await this.blobServiceClient.getContainerClient(
+      "unparsed-demos"
+    );
+    await container.createIfNotExists();
+    return container;
+  }
 }
