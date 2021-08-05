1b0166fdb05901abd9e14e24d2b1b14daf3d72f1
Alexander Holdt <ave@cvation.com>
2021-08-05 23:57:31 +0200

    fixes function project referecing nest library project

--- functions/unparsed-demos-blobtrigger/index.ts
+++ functions/unparsed-demos-blobtrigger/index.ts
@@ -1,8 +1,14 @@
 import { AzureFunction, Context } from "@azure/functions"
+import { NestFactory } from "@nestjs/core";
+import { DemoparserModule } from "../../libs/demoparser/src/demoparser.module";
+import { DemoparserService } from "../../libs/demoparser/src/demoparser.service";
 
 const blobTrigger: AzureFunction = async function (context: Context): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    console.log(context);
+    const app = await NestFactory.create(DemoparserModule);
+    await app.init();
+    const test = app.get(DemoparserService)
+    console.log("sdfsdsdfsdff");
+    test.parseDemo();
 };
 
 export default blobTrigger;
