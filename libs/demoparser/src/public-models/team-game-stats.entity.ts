1c450c118823eb8e9ebe780432a6bfae7ed56233
Alexander Holdt <ave@cvation.com>
2021-10-23 22:51:45 +0200

    multiply percentages by 100

--- libs/demoparser/src/public-models/team-game-stats.entity.ts
+++ libs/demoparser/src/public-models/team-game-stats.entity.ts
@@ -10,17 +10,20 @@ export class TeamGameStats {
 
     this.id = `${this.objectType}_${this.teamName}_${this.gameId}_${this.side}`;
 
-    this.roundWinPct = teamRoundStats.reduce((total, next) => total + (next.roundWon ? 1 : 0), 0) / teamRoundStats.length;
+    this.roundWinPct = (teamRoundStats.reduce((total, next) => total + (next.roundWon ? 1 : 0), 0) / teamRoundStats.length) * 100;
 
-    this.multiKillPct = teamRoundStats.reduce((total, next) => total + next.multiKills, 0) / teamRoundStats.length;
-    this.openingKillPct = teamRoundStats.reduce((total, next) => total + next.openingKills, 0);
+    this.multiKillPct = (teamRoundStats.reduce((total, next) => total + next.multiKills, 0) / teamRoundStats.length) * 100;
+    this.openingKillPct = teamRoundStats.reduce((total, next) => total + next.openingKills, 0) * 100;
 
     this.fiveVsFourWinPct =
-      teamRoundStats.reduce((total, next) => total + (next.fiveVsFourRound && next.fiveVsFourWin ? 1 : 0), 0) / teamRoundStats.length;
+      (teamRoundStats.reduce((total, next) => total + (next.fiveVsFourRound && next.fiveVsFourWin ? 1 : 0), 0) / teamRoundStats.length) *
+      100;
     this.fourVsFiveWinPct =
-      teamRoundStats.reduce((total, next) => total + (next.fourVsFiveRound && next.fourVsFiveWin ? 1 : 0), 0) / teamRoundStats.length;
+      (teamRoundStats.reduce((total, next) => total + (next.fourVsFiveRound && next.fourVsFiveWin ? 1 : 0), 0) / teamRoundStats.length) *
+      100;
     this.tradePct =
-      teamRoundStats.reduce((total, next) => (total + next.deaths > 0 ? next.deathsTraded / next.deaths : 1), 0) / teamRoundStats.length;
+      (teamRoundStats.reduce((total, next) => (total + next.deaths > 0 ? next.deathsTraded / next.deaths : 1), 0) / teamRoundStats.length) *
+      100;
 
     this.molotovAdr = teamRoundStats.reduce((total, next) => total + next.molotovAdr, 0) / teamRoundStats.length;
     this.heAdr = teamRoundStats.reduce((total, next) => total + next.heAdr, 0) / teamRoundStats.length;
