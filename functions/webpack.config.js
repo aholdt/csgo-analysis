baa3a88d11712914655709d4af51f6f79dee8530
Alexander Holdt <ave@cvation.com>
2021-10-28 23:51:58 +0200

    added webpack to functions so it can be deployed

---
+++ functions/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  target: "node", // IMPORTANT!
+  entry: {
+    UnparsedDemosBlobTrigger: path.join(__dirname, "unparsed-demos-blobtrigger/index.ts"),
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs", // IMPORTANT!
+  },
+  plugins: [
+    new webpack.IgnorePlugin({
+      checkResource(resource) {
+        const lazyImports = [
+          "@nestjs/microservices",
+          "@nestjs/websockets",
+          "@nestjs/websockets/socket-module",
+          "@nestjs/microservices/microservices-module",
+          "memcpy",
+        ];
+        if (!lazyImports.includes(resource)) {
+          return false;
+        }
+        try {
+          require.resolve(resource);
+        } catch (err) {
+          return true;
+        }
+        return false;
+      },
+    }),
+    new CopyWebpackPlugin([{ context: "", from: "**/function.json", to: "" }, "host.json", "local.settings.json"]),
+  ],
+
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+};
