eddfb8e55cba976c7fe1c31be2593d53d1dd219a
Alexander Holdt <ave@cvation.com>
2021-11-03 00:15:34 +0100

    Revert "remove local settings from copy task"
    This reverts commit 072cfd93e5da92d9ddebd9d3961c286326364c3e.

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = function (options) {
-  const toCopy = [
-    { context: path.resolve(__dirname), from: "**/function.json" },
-    { context: path.resolve(__dirname), from: "**/host.json" },
-  ];
   return {
     ...options,
     plugins: [
       ...options.plugins,
       new CopyWebpackPlugin({
-        patterns: toCopy,
+        patterns: [
+          { context: path.resolve(__dirname), from: "**/function.json" },
+          { context: path.resolve(__dirname), from: "**/host.json" },
+          { context: path.resolve(__dirname), from: "**/local.settings.json" },
+        ],
       }),
     ],
     output: {
